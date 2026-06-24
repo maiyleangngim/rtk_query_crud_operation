8576498834bc1168182d3ba4f41782838d04145c
Maiy Leangnim <2024020maiy@aupp.edu.kh>
2026-06-24 13:28:20 +0700

    find error

--- src/app/table-data/page.tsx
+++ src/app/table-data/page.tsx
@@ -54,7 +54,7 @@ export default function DataTablePage() {
     ],
     categoryUuid: "462d9f60-8346-45ab-b8b3-a597d240965b",
     supplierUuid: "a34496d2-370e-4332-8c6d-b4a6bc069bf1",
-    brandUuid: " 8f2e3bcb-bb0b-45a1-b9bc-1d43f08f0ddb",
+    brandUuid: "8f2e3bcb-bb0b-45a1-b9bc-1d43f08f0ddb",
   };
 
   const [updateProductByUUID] = useUpdateProductbyUUIDMutation();
